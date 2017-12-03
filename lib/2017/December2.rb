946de7c15d97bf43af4838ae3a1cd8321a52ccc8
Sven Schoradt <sven.schoradt@googlemail.com>
2017-12-03 21:58:30 +0100

    SYS: add documentation

--- lib/2017/December2.rb
+++ lib/2017/December2.rb
@@ -1,6 +1,11 @@
-
-
+##
+# This class is the advent of code excercise for December the 2 2017.
 class December2
+  ##
+  # Compute the checksum of the spredsheet.
+  #
+  # The checksum of the spreadsheet is the sum of the difference of 
+  # the maximum and the minimum of each line.
   def December2.checksum(text)
     sum = 0
 
@@ -15,6 +20,8 @@ class December2
     sum
   end
 
+  ##
+  # The functions parses the +text+ into an array of arrays representing the spreadsheet.
   def December2.parseSpreadsheet(text)
     ss = [];
 
@@ -25,6 +32,8 @@ class December2
     ss
   end
 
+  ##
+  # The functions parses +line+ into an array of integers.
   def December2.parseLine(line)
     line.split(" ").map(&:to_i)
   end
