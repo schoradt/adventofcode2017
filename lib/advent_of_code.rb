b0e6fad27c56008f99ba1efb5cbfac3715cd2c93
Sven Schoradt <s.schoradt@infotec-edv.de>
2017-12-12 11:10:02 +0100

    FIX: add day 4 results

--- lib/advent_of_code.rb
+++ lib/advent_of_code.rb
@@ -1,6 +1,7 @@
 require_relative './2017/december_1'
 require_relative './2017/december_2'
 require_relative './2017/december_3'
+require_relative './2017/december_4'
 
 ##
 # This class is the main entry point for the advent of code in ruby
